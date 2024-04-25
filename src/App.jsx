9c4fc216a7d776d44826c358f84092a0abbd81ad
HimanshuBungla <https://github.com/HimanshuBungla>
2024-04-25 17:18:25 +0530

    game commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
+
+
+
+function App() {
+
+  const [isGameStarted,setIsGameStarted] = useState(false);
+
+  // const toggleGamePlay = ()=>{
+  //   setIsGameStarted((prev)=>!prev);
+  // };
+  return (
+    <>
+    {isGameStarted ? <GamePlay /> :<StartGame
+    
+    // toggle = {toggleGamePlay}
+    toggle = {() => setIsGameStarted((prev)=>!prev)}
+    
+    />}
+    </>
+  )
+}
+
+export default App;
